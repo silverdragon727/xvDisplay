17cdc9c0e4fcee6ce059a9ee19c85c155df58cea
jhk001 <jhk001@users.noreply.github.com>
2013-09-21 08:30:07 +0800

    [AD] Add dedicated Modules.
    'Extensions': Place extension methods within this Module.
    'NativeMethods': Put P/Invoke methods in this Module.

---
+++ xvDisplay/Extensions.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Runtime.CompilerServices
+
+Friend Module Extensions
+
+    <Extension>
+    Function HasValue(ByVal s As String) As Boolean
+        Return Not String.IsNullOrEmpty(s)
+    End Function
+
+End Module
---
+++ xvDisplay/NativeMethods.vb
@@ -0,0 +1,6 @@
+﻿Friend Module NativeMethods
+
+    Declare Function GetTickCount Lib "kernel32" () As UInt32
+    ' Can be replaced by Environment.TickCount
+
+End Module
