f66daa20fce8350246f701d3fa5b4a3d38caa268
jhk001 <jhk001@users.noreply.github.com>
2013-09-21 12:44:00 +0800

    [UP] Optimize Forms' code-behind.

--- xvDisplay/DebugForm.vb
+++ xvDisplay/DebugForm.vb
@@ -1,31 +1,36 @@
-﻿Public Class DebugForm
+﻿Public NotInheritable Class DebugForm
 
-    Public IfClose As Boolean = False
+    Friend Shared closeFlag As Boolean = False
 
-    Private Sub DebugForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        TextBox1.Top = -2
-        TextBox1.Left = -2
-        TextBox1.Height = Me.ClientSize.Height + 4
-        TextBox1.Width = Me.ClientSize.Width + 4
+    Private Sub OnLoaded(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        OutputTextBox.Top = -2
+        OutputTextBox.Left = -2
+        OutputTextBox.Height = Me.ClientSize.Height + 4
+        OutputTextBox.Width = Me.ClientSize.Width + 4
     End Sub
 
     Public Sub Print(ByVal str As String)
-        TextBox1.AppendText(String.Format("[{0}] ", Date.Now.ToString("HH:mm:ss.fff")))
-        If Not str.EndsWith(vbCrLf) Then
-            str += vbCrLf
+        OutputTextBox.AppendText(Date.Now.ToString("[HH:mm:ss.fff] "))
+        ' OutputTextBox.AppendText(String.Format("[{0}] ", Date.Now.ToString("HH:mm:ss.fff")))
+        ' If Not str.EndsWith(vbCrLf) Then
+        ' str += vbCrLf
+        ' End If
+        ' 不要通过 += 运算符连接字符串 (性能问题)。此处可直接 AppendText。
+        OutputTextBox.AppendText(str)
+        If Not str.EndsWith(vbCrLf, StringComparison.OrdinalIgnoreCase) Then
+            OutputTextBox.AppendText(vbCrLf)
         End If
-        TextBox1.AppendText(str)
     End Sub
 
-    Private Sub DebugForm_Resize(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Resize
-        TextBox1.Height = Me.ClientSize.Height + 4
-        TextBox1.Width = Me.ClientSize.Width + 4
+    Protected Overrides Sub OnResize(e As EventArgs)
+        OutputTextBox.Height = Me.ClientSize.Height + 4
+        OutputTextBox.Width = Me.ClientSize.Width + 4
+        MyBase.OnResize(e)
     End Sub
 
-    Private Sub DebugForm_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+    Protected Overrides Sub OnFormClosing(e As FormClosingEventArgs)
         Me.Hide()
-        If Not IfClose Then
-            e.Cancel = True
-        End If
+        e.Cancel = Not closeFlag
+        MyBase.OnFormClosing(e)
     End Sub
 End Class
--- xvDisplay/StageForm.vb
+++ xvDisplay/StageForm.vb
@@ -1,28 +1,32 @@
 ﻿Public Class StageForm
+
     Dim ctrl As Controller
 
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    Private Sub OnLoaded(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         SBSLibrary.StandardIO.Output = AddressOf DebugForm.Print
-        ctrl = New Controller(Me)
+        ctrl = New Controller()
         ctrl.Start()
     End Sub
 
-    Private Sub Form1_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles MyBase.KeyPress
-        If e.KeyChar = "c" Then
-            ctrl.Dispose()
-            ctrl = New Controller(Me)
-            ctrl.Start()
-        ElseIf e.KeyChar = "l" Then
-            For i As Integer = 0 To 100
+    Protected Overrides Sub OnKeyPress(e As KeyPressEventArgs)
+        Select Case e.KeyChar
+            Case "c"c
                 ctrl.Dispose()
-                ctrl = New Controller(Me)
+                ctrl = New Controller()
                 ctrl.Start()
-            Next
-        ElseIf e.KeyChar = "d" Then
-            DebugForm.Visible = Not DebugForm.Visible
-        ElseIf e.KeyChar = "q" Then
-            DebugForm.IfClose = True
-            Application.Exit()
-        End If
+            Case "d"c
+                DebugForm.Visible = Not DebugForm.Visible
+            Case "l"c
+                For i As Integer = 0 To 100
+                    ctrl.Dispose()
+                    ctrl = New Controller()
+                    ctrl.Start()
+                Next
+            Case "q"c
+                DebugForm.closeFlag = True
+                Application.Exit()
+        End Select
+
+        MyBase.OnKeyPress(e)
     End Sub
 End Class
